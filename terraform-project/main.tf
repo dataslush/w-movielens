5bb45656f70616fbd0c44ebee634cc14f5754bfa
Dminor7 <contact.dshukla@gmail.com>
2024-01-05 18:08:53 +0530

    Formatting code

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -5,14 +5,14 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "movielens-bucket" {
-  name          = "dataslush-waltlabs-movielens"
-  location      = var.region
-  force_destroy = true
+  name                     = "dataslush-waltlabs-movielens"
+  location                 = var.region
+  force_destroy            = true
   public_access_prevention = "enforced"
 }
 
 resource "google_bigquery_dataset" "movielens-dataset" {
   dataset_id = "dataslush_waltlabs_movielens"
   project    = var.project_id
-  location = var.region
+  location   = var.region
 }
